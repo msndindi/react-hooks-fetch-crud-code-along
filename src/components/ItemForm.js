492f42b8635eebb86369a7a504dab73d4b626602
Mutindi Hope <msndindi@outlook.com>
2023-04-29 01:01:17 +0300

    Completed Assignment

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,37 +1,56 @@
-import React, { useState } from "react";
 
-function ItemForm() {
-  const [name, setName] = useState("");
-  const [category, setCategory] = useState("Produce");
+      
+ import React, { useState } from "react";
 
-  return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-
-      <label>
-        Category:
-        <select
-          name="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
-
-      <button type="submit">Add to List</button>
-    </form>
-  );
-}
-
-export default ItemForm;
+ function ItemForm({onAddItem}) {
+   const [name, setName] = useState("");
+   const [category, setCategory] = useState("Produce");
+ 
+   function handleSubmit(e){
+     e.preventDefault()
+     const itemData = {
+       name:name,
+       category:category,
+       isInCart:false
+     }
+     fetch("http://localhost:4000/items", {
+       method: "POST",
+       headers: {"content-type":"application/json"},
+       body: JSON.stringify(itemData)
+     })
+     .then(res=>res.json())
+     .then((newItem)=>onAddItem(newItem))
+ 
+   }
+ 
+   return (
+     <form className="NewItem" onSubmit={handleSubmit}>
+       <label>
+         Name:
+         <input
+           type="text"
+           name="name"
+           value={name}
+           onChange={(e) => setName(e.target.value)}
+         />
+       </label>
+ 
+       <label>
+         Category:
+         <select
+           name="category"
+           value={category}
+           onChange={(e) => setCategory(e.target.value)}
+         >
+           <option value="Produce">Produce</option>
+           <option value="Dairy">Dairy</option>
+           <option value="Dessert">Dessert</option>
+         </select>
+       </label>
+ 
+       <button type="submit">Add to List</button>
+     </form>
+   );
+ }
+ 
+ export default ItemForm;
